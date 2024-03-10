a8038022b074bbca7dfa70426cf71c67d9c61f2d
Syed Talha <syedt.in00@gmail.com>
2024-03-10 23:46:34 +0500

    create funcnality added

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,35 +1,64 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 function Create() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post("https://65ec22c80ddee626c9afa2b3.mockapi.io/crud", {
+        name: name,
+        age: age,
+        email: email
+    })
+}
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-4">
         <div className="p-4 text-center">
-            <h1>
-                Create Date
-            </h1>
+          <h1>Create Date</h1>
         </div>
-        <form>
-           <div className="form-group">
-              <label htmlFor="nameInp">Enter Name:</label>
-              <input type="text" placeholder="Name" className="form-control" id="nameInp"/>
-           </div>
-           <div className="form-group">
-              <label htmlFor="ageInp">Enter Age:</label>
-              <input type="number" placeholder="Age" className="form-control" id="ageInp"/>
-           </div>
-           <div className="form-group">
-              <label htmlFor="emailInp">Enter Email:</label>
-              <input type="email" placeholder="Email" className="form-control" id="emailInp"/>
-           </div>
-           <br />
-           <div className="d-grid">
-           <input type="submit" value="Submit" className="btn btn-primary" />
-           </div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="nameInp">Enter Name:</label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="form-control"
+              id="nameInp"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="ageInp">Enter Age:</label>
+            <input
+              type="number"
+              placeholder="Age"
+              className="form-control"
+              id="ageInp"
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="emailInp">Enter Email:</label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="form-control"
+              id="emailInp"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <br />
+          <div className="d-grid">
+            <input type="submit" value="Submit" className="btn btn-primary" />
+          </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Create
+export default Create;
